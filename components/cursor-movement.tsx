e1c7bbd0df5eca8dce6f0cdc70f5170261996031
Aryan Kalra <aryan.smart832@gmail.com>
2025-08-13 22:05:10 +0530

    add pencil svg icon

--- components/cursor-movement.tsx
+++ components/cursor-movement.tsx
@@ -34,51 +34,48 @@ function CursorMovement() {
   }, []);
 
   return (
-    <div
-      style={{
-        width: "50px",
-        height: "50px",
-        position: "fixed",
-        borderRadius: "23px",
-        pointerEvents: "none",
-        zIndex: 999999,
-        cursor: "none",
-        // transition: "transform 0.04s ease-in-out",
-        transform: `translate(${userCursor.x - 10}px, ${userCursor.y - 30}px)`,
-      }}
-    >
+    <>
       <div
         style={{
-          width: "25px",
-          height: "25px",
-          // color: position?.cursorStyle,
+          width: "5px",
+          height: "5px",
+          borderRadius: "50%",
+          position: "fixed",
+          transition: "transform 0.02s ease-in-out",
+          backgroundColor: "black",
+          transform: `translate(${userCursor.x - 4}px, ${userCursor.y - 4}px)`,
         }}
-        className=" relative top-0 left-0 mx-auto"
+      />
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          position: "fixed",
+          borderRadius: "23px",
+          pointerEvents: "none",
+          zIndex: 999999,
+          cursor: "none",
+          // transition: "transform 0.04s ease-in-out",
+          transform: `translate(${userCursor.x}px, ${userCursor.y}px)`,
+        }}
       >
-        {/* <div
-          className={`absolute -top-11 min-w-[150px] bg-lime-300/90 ${
-            messages.message && "p-1"
-          } text-black rounded-[5px] text-sm`}
-        >
-          {messages.name !== name && messages.message && "Typing..."}
-        </div> */}
+        <div
+          style={{
+            WebkitMaskImage: "url('/pencil.svg')",
+            WebkitMaskRepeat: "no-repeat",
+            rotate: "90deg",
+            WebkitMaskSize: "contain",
+            WebkitMaskPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundColor: "black",
+            width: "30px",
+            height: "30px",
+          }}
+        />
       </div>
-      <div
-        style={{
-          WebkitMaskImage: "url('/pointer.svg')",
-          WebkitMaskRepeat: "no-repeat",
-          WebkitMaskSize: "contain",
-          WebkitMaskPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundColor: "purple",
-          width: "25px",
-          height: "25px",
-        }}
-        className="relative top-0 left-0 mx-auto"
-      ></div>
-    </div>
+    </>
   );
 }
 
